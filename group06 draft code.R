da2f9458e95c13aa51fa48d3348f529eec8740fe
yawen-f <3025872f@student.gla.ac.uk>
2025-03-14 14:20:19 +0000

    Update group06 draft code.R
    1. modify boxplot: log(votes) and genre
    2. add model4 (interaction) can be used for further work

--- group06 draft code.R
+++ group06 draft code.R
@@ -5,19 +5,21 @@ library(knitr)
 library(janitor)
 library(stats)
 library(GGally)
+library(car)
 
 dataset <- read.csv("dataset06.csv")
 
 dataset <- dataset %>%
   drop_na()
-
+#binary: rating > 7 is set to 1, rating <= 7 is set to 0)
 dataset$rating_new <- ifelse(dataset$rating > 7, 1, 0)
+#Convert categorical variables to factor variables
 dataset$rating_new <- as.factor(dataset$rating_new)
 dataset$genre <- as.factor(dataset$genre)
 
 str(dataset)
 
-# EDA
+##############Exploratory Data Analysis##############
 ggplot(data = dataset, aes(x = rating_new, y = year, fill = rating_new)) +
   geom_boxplot() +
   labs(x = "rating_new", y = "year")+ 
@@ -39,6 +41,14 @@ ggplot(data = dataset, aes(x = rating_new, y = votes, fill = rating_new)) +
   labs(x = "rating_new", y = "votes")+ 
   theme(legend.position = "none")
 
+#log(votes)
+ggplot(data = dataset, aes(x = rating_new, y = log(votes + 1), fill = rating_new)) +
+  geom_boxplot() +
+  labs(x = "rating_new", 
+       y = "log(votes + 1)", 
+       title = "Log-Transformed Votes vs Rating Category") + 
+  theme(legend.position = "none")
+
 # categorical explanatory variable
 dataset %>% 
   tabyl(genre, rating_new) %>% 
@@ -49,16 +59,35 @@ dataset %>%
 ggplot(data = dataset, aes(x = rating_new, group = genre)) +
   geom_bar(aes(y = ..prop.., fill = genre), stat = "count", position = "dodge") +
   labs(x = "Rating", y = "Proportion")
+#boxplot-genre
+ggplot(dataset, aes(x = genre, y = rating, fill = genre)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+###############formal analysis ###############
+###model selection
+levels(dataset$rating_new) #baseline is 0(rating lower than 7))
 
-# formal analysis (baseline is 0(lower than 7))
+#model1-full model: rating_new ~ year + length + budget + votes + genre
 model1 <- glm(rating_new ~ year + length + budget + votes + genre, 
                   data = dataset, 
                   family = binomial(link = "logit"))
 model1 %>%
   summary()
 
+vif(model1)
+
+summ(model1)
 confint(model1) %>%
+  kable()
+#model2-stepwise: rating_new ~ year + length + budget + genre
+model2 <- step(model1, direction = "both", trace = TRUE)
+summary(model2)
+
+vif(model2)
+
+summ(model2)
+confint(model2) %>%
   kable()
 
 # Log-odds
@@ -137,6 +166,16 @@ plot(roc2, col = "red", add = TRUE)
 plot(roc3, col = "black", add = TRUE)
 legend("bottomright", legend = c("Model1", "Model2", "Model3"), col = c("blue", "red", "black"), lwd = 2)
 
-
+#model4-interaction without votes：
+model4 <- glm(rating_new ~ year + length + budget * genre, 
+              data = dataset, 
+              family = binomial(link = "logit"))
+summary(model6)
+anova(model6, test = "Chisq")
+vif(model6)
+#AIC：901.84 but year not significant in anova
+summ(model6)
+confint(model6) %>%
+  kable()
 
 
